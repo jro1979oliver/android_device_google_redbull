c11e8842a9721fea75a86c88de3f1727ada068d4
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2024-04-18 14:42:46 +0300

    redbull: disable lmkd stats logging
    * We`re not debugging lmk daemon, so this is redundant

--- device-common.mk
+++ device-common.mk
@@ -723,7 +723,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.systemuicompilerfilter=speed
 
 # Enable stats logging in LMKD
-TARGET_LMKD_STATS_LOG := true
+TARGET_LMKD_STATS_LOG := false
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.gadgethal.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.gadgethal.sh
@@ -855,7 +855,7 @@ persist.vendor.bt.aac_vbr_frm_ctl.enabled=true
 # Set lmkd options
 PRODUCT_PRODUCT_PROPERTIES += \
         ro.config.low_ram ?= false \
-        ro.lmk.log_stats = true \
+        ro.lmk.log_stats = false \
 
 # charger
 PRODUCT_PRODUCT_PROPERTIES += \
